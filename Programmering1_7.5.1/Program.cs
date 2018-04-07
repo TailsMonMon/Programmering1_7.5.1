3028d4c9cd69f0dcefef28e3e9fa7fbc98a3cc71
Monica Björk <m.bjork95@gmail.com>
2018-04-07 15:31:56 +0200

    Add project files.

---
+++ Programmering1_7.5.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_7._5._1 {
+    class Program {
+        static void Main(string[] args) {
+            int num = 1;
+
+            do {
+                Console.WriteLine(num++);
+            } while(num <= 20);
+            Console.ReadLine();
+        }
+    }
+}
